46e60cd6ae9188180d3db3620672a07983dbed04
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2025-11-12 14:50:29 -0700

    wip

---
+++ Makefile
@@ -0,0 +1,44 @@
+SHELL := /bin/bash
+RCMDBATCHVANILLA := R CMD BATCH --vanilla
+RVANILLA := R --vanilla --quiet
+RSCRIPTVANILLA := Rscript --vanilla --quiet
+
+N := 10 100 1000
+ITR :=  $(shell seq 1 1000)
+METHODS := via_base_matrix via_tidyverse via_data.table via_stats_reshape via_reduce_merge
+BENCHMARKS := $(foreach m,$(METHODS),$(foreach i,$(ITR),$(foreach n,$(N),$(m)/$(n)/$(i).dput)))
+
+BENCHMARKPLOTS := benchmarks_n10.png benchmarks_n100.png benchmarks_n1000.png benchmarks_n10000.png benchmarks_n100000.png
+
+CRAN = 'https://cran.rstudio.com'
+
+.INTERMEDIATE: slides.Rmd
+.PHONY: all clean
+
+all : .pkgs slides.html $(BENCHMARKS)
+
+.pkgs :
+	R --quiet --vanilla -e "options(repos = $(CRAN))"\
+		-e "if (!('devtools' %in% rownames(installed.packages()))) {install.packages('devtools')}"\
+		-e "devtools::install_cran('knitr')"\
+		-e "devtools::install_cran('rmarkdown')"
+		-e "devtools::install_cran('data.table')"
+		-e "devtools::install_cran('tidyverse')"
+	touch .pkgs
+
+%.Rout: %.R
+	$(RCMDBATCHVANILLA) $< $@
+
+%.dput: benchmarks.R methods.R
+	$(RSCRIPTVANILLA) $< $*
+
+#benchmarks_n%.png: benchmarks.R methods.R
+#	$(RSCRIPTVANILLA) $< $*
+
+slides.html : slides.Rmd style.css #$(BENCHMARKPLOTS)
+	$(RVANILLA) -e "rmarkdown::render('$<')"
+
+clean:
+	$(RM) .pkgs
+	$(RM) *.html
+	$(RM) *.png
---
+++ methods.R
@@ -0,0 +1,183 @@
+################################################################################
+# file: methods.R
+#
+# define several methods for use in a presentation to the Devner R User Group on
+# using tidyverse, data.table, and base R method for building a inidcatior 
+################################################################################
+
+generate_data <- function(n.patients = 10L) {
+  # for each patient, generate a random number of encounters (at least one
+  # encounter for each patient)
+  encounters <- rpois(n = n.patients, lambda = 4.5) + 1L
+
+  # for each encounter generate a set of random "conditions"
+  n.conditions <- rpois(n = sum(encounters), lambda = 2)
+
+  # generate the conditions
+  conditions <- lapply(n.conditions, sample, x = LETTERS, replace = TRUE)
+  conditions <- lapply(conditions, function(x) {if (length(x)) x else ""})
+  n.conditions <- lapply(conditions, length)
+
+  pid <- rep.int(seq_len(n.patients), times = encounters)
+  pid <- rep.int(pid, times = n.conditions)
+
+  eid <- do.call(c, lapply(encounters, seq_len))
+  eid <- rep.int(eid, times = n.conditions)
+
+  data.frame(pid = pid, eid = eid, condition = do.call(c, conditions))
+}
+
+check_inputs <- function(data, id.vars, conditions) {
+  stopifnot(inherits(data, "data.frame"))
+  stopifnot(inherits(id.vars, "character"), length(id.vars) > 0L)
+  stopifnot(inherits(conditions, "character"), length(conditions) == 1L)
+  stopifnot(all(id.vars %in% names(data)), conditions %in% names(data))
+  invisible(TRUE)
+}
+
+via_tidyverse <- function(data, id.vars, conditions) {
+  check_inputs(data, id.vars, conditions)
+  stopifnot(!inherits(data, "data.table"))
+
+  rtn <-
+    tidyr::pivot_wider(
+      data        = dplyr::filter(data, .data[[conditions]] %in% LETTERS[1:12]),
+      id_cols     = tidyselect::all_of(id.vars),
+      names_from  = tidyselect::all_of(conditions),
+      values_from = tidyselect::all_of(conditions),
+      values_fill = 0L,
+      values_fn   = function(x) { as.integer(length(x) > 0) }
+    )
+
+  # verify all conditions have been flagged
+  for (j in LETTERS[which(!(LETTERS[1:12] %in% names(rtn)))]) {
+    rtn[[j]] <- rep(0L, nrow(rtn))
+  }
+
+  iddf <- dplyr::distinct(dplyr::select(data, tidyselect::all_of(id.vars)))
+  rtn <- dplyr::left_join(x = iddf, y = rtn, by = id.vars)
+
+  rtn <-
+    dplyr::mutate(rtn, dplyr::across(tidyselect::everything(), ~ tidyr::replace_na(.x, 0L)))
+
+  rtn <-
+    dplyr::select(rtn, tidyselect::all_of(id.vars), tidyselect::all_of(LETTERS[1:12]))
+
+  rtn
+}
+
+via_data.table <- function(data, id.vars, conditions) {
+  check_inputs(data, id.vars, conditions)
+  stopifnot(inherits(data, 'data.table'))
+
+  rtn <- data.table::dcast(
+    data = unique(subset(data, data[[conditions]] %in% LETTERS[1:12])),
+    formula = as.formula(paste(paste(id.vars, collapse = "+"), "~", conditions)),
+    fun.aggregate = function(x) { as.integer(length(x) > 0) },
+    value.var = conditions,
+    fill = 0L
+  )
+
+  # verify all conditons have been flagged
+  for (j in LETTERS[which(!(LETTERS[1:12] %in% names(rtn)))]) {
+    data.table::set(rtn, j = j, value = rep(0L, nrow(rtn)))
+  }
+
+  iddf <- unique(subset(data, subset = TRUE, select = id.vars))
+
+  rtn <- merge(x = iddf, y = rtn, all.x = TRUE, by = names(iddf))
+  rtn[is.na(rtn)] <- 0L
+
+  data.table::setcolorder(rtn, c(id.vars, LETTERS[1:12]))
+
+  rtn
+}
+
+via_stats_reshape <- function(data, id.vars, conditions) {
+  check_inputs(data, id.vars, conditions)
+
+  rtn <-
+    stats::reshape(
+      data = unique(subset(data, data[[conditions]] %in% LETTERS[1:12])),
+      idvar = id.vars,
+      timevar = conditions,
+      v.names = conditions,
+      direction = "wide"
+    )
+
+  for (j in names(rtn)[-which(names(data) %in% id.vars)]) {
+    flag <- as.integer(!is.na(rtn[[j]]))
+    if (length(flag) == 0) {
+      flag <- rep(0L, nrow(rtn))
+    }
+    rtn[[j]] <- flag
+  }
+
+  names(rtn) <- sub(pattern = paste0(conditions, "."), "", names(rtn), fixed = TRUE)
+
+  # verify all conditons have been flagged
+  for (j in LETTERS[which(!(LETTERS[1:12] %in% names(rtn)))]) {
+    rtn[[j]] <- rep(0L, nrow(rtn))
+  }
+
+  colorder <- c(id.vars, LETTERS[1:12])
+
+  rtn <-
+    merge(
+      x = unique(data[, id.vars, drop = FALSE]),
+      y = rtn[, colorder, drop = FALSE],
+      all.x = TRUE,
+      by = id.vars
+    )
+
+  rtn[is.na(rtn)] <- 0L
+  rtn
+}
+
+via_reduce_merge <- function(data, id.vars, conditions) {
+  check_inputs(data, id.vars, conditions)
+  iddf <- unique(data[, id.vars, drop = FALSE])
+
+  cnds <- lapply(LETTERS[1:12], function(x) {
+    idx <- data[[conditions]] == x
+    rtn <- unique(data[idx, id.vars, drop = FALSE])
+    rtn[[x]] <- rep(1L, nrow(rtn))
+    rtn
+    })
+
+  cnds <- Reduce(
+    f = function(x, y) { merge(x, y, all = TRUE, by = id.vars) },
+    x = cnds,
+    init = iddf
+  )
+
+  cnds[is.na(cnds)] <- 0L
+  cnds
+}
+
+via_base_matrix <- function(data, id.vars, conditions) {
+  check_inputs(data, id.vars, conditions)
+
+  iddf <- unique(data[, id.vars, drop = FALSE])
+  cnds <- unique(subset(data, data[[conditions]] %in% LETTERS[1:12]))
+
+  X <- matrix(0L, nrow = nrow(iddf), ncol = 12)
+  colnames(X) <- LETTERS[1:12]
+
+  key_iddf <- do.call(paste, c(iddf, sep = "\r"))
+  key_cnds <- do.call(paste, c(cnds[, id.vars, drop = FALSE], sep = "\r"))
+
+  ri <- match(key_cnds, key_iddf)
+  ci <- match(cnds[[conditions]], LETTERS[1:12])
+  keep <- !(is.na(ri) | is.na(ci))
+  if (any(keep)) {
+    X[cbind(ri[keep], ci[keep])] <- 1L
+  }
+
+  cbind(iddf, as.data.frame(X, check.names = FALSE))
+}
+
+
+
+
+
