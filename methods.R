f582668b5e924b520777431c82ca18be1869a8d8
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2025-11-12 16:17:31 -0700

    update methods.R

--- methods.R
+++ methods.R
@@ -39,29 +39,34 @@ via_tidyverse <- function(data, id.vars, conditions) {
   check_inputs(data, id.vars, conditions)
   stopifnot(!inherits(data, "data.table"))
 
+  cond_levels <- LETTERS[1:12]
+
+  prep <- dplyr::filter(data, .data[[conditions]] %in% cond_levels)
+  prep[[conditions]] <- factor(prep[[conditions]], levels = cond_levels)
+  prep[["flag"]] <- 1L
+
   rtn <-
     tidyr::pivot_wider(
-      data        = dplyr::filter(data, .data[[conditions]] %in% LETTERS[1:12]),
+      data        = prep,
       id_cols     = tidyselect::all_of(id.vars),
       names_from  = tidyselect::all_of(conditions),
-      values_from = tidyselect::all_of(conditions),
+      values_from = "flag",
       values_fill = 0L,
-      values_fn   = function(x) { as.integer(length(x) > 0) }
+      values_fn   = max,
+      names_expand = TRUE
     )
 
-  # verify all conditions have been flagged
-  for (j in LETTERS[which(!(LETTERS[1:12] %in% names(rtn)))]) {
-    rtn[[j]] <- rep(0L, nrow(rtn))
-  }
-
-  iddf <- dplyr::distinct(dplyr::select(data, tidyselect::all_of(id.vars)))
+  iddf <- dplyr::distinct(
+    data,
+    dplyr::across(tidyselect::all_of(id.vars))
+  )
   rtn <- dplyr::left_join(x = iddf, y = rtn, by = id.vars)
 
   rtn <-
-    dplyr::mutate(rtn, dplyr::across(tidyselect::everything(), ~ tidyr::replace_na(.x, 0L)))
+    dplyr::mutate(rtn, dplyr::across(tidyselect::all_of(cond_levels), ~ tidyr::replace_na(.x, 0L)))
 
   rtn <-
-    dplyr::select(rtn, tidyselect::all_of(id.vars), tidyselect::all_of(LETTERS[1:12]))
+    dplyr::select(rtn, tidyselect::all_of(id.vars), tidyselect::all_of(cond_levels))
 
   rtn
 }
@@ -185,6 +190,5 @@ via_base_matrix <- function(data, id.vars, conditions) {
 
   cbind(iddf, as.data.frame(X, check.names = FALSE))
 }
-
 
 
